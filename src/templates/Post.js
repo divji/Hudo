63e13a5e8c0f1104f3b59bed1f2829df99abb2a7
Vladislav Kolesnik <63100043+divji@users.noreply.github.com>
2020-09-24 21:02:18 +0300

    Update Post.js

--- src/templates/Post.js
+++ src/templates/Post.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import styled from "styled-components";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 
 export const Container = styled.div`
     margin: auto;
